b3cadddf7e87c1cdffda90b43b52fea536731771
Shiva Singh Shishodia <130183426+shivasinghshishodia@users.noreply.github.com>
2026-04-06 18:21:42 +0530

    Time: 27 ms (67.69%) | Memory: 12.4 MB (91.99%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        x, y = 0, 0
+        
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'R':
+                x += 1
+            else:  # 'L'
+                x -= 1
+        
+        return x == 0 and y == 0
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        
